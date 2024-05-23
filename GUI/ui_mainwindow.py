8bd7b46137bfbb1862dad4b0fec41f53b0e38e6a
jiangjiasheng <jiangjiasheng@pingansec.com>
2024-05-23 17:05:04 +0800

    update & fix: QueueManger

--- GUI/ui_mainwindow.py
+++ GUI/ui_mainwindow.py
@@ -13,7 +13,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(920, 635)
+        MainWindow.resize(1200, 635)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
