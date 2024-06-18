5163037fff0a00fcf79dbff7900b672c5ceef9f9
json <jiangjiasheng@pingansec.com>
2024-06-18 13:15:49 +0800

    add: kaobei(more reliable than 90mh)

--- ComicSpider/middlewares.py
+++ ComicSpider/middlewares.py
@@ -41,6 +41,23 @@ class ComicspiderDownloaderMiddleware(object):
         spider.logger.info(f'Spider opened: 【{spider.name}】')
 
 
+class KaobeiMiddleware(ComicspiderDownloaderMiddleware):
+    def process_request(self, request, spider):
+        request.headers.update({
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0",
+            "Accept": "application/json",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Accept-Encoding": "gzip",
+            "Content-Encoding": "gzip",
+            "platform": "1",
+            "version": "2024.01.08",
+            "webp": "1",
+            "region": "1",
+            "Origin": "https://www.mangacopy.com",
+        })
+        return None
+
+
 class ComicDlProxyMiddleware(ComicspiderDownloaderMiddleware):
     """使用情况是“通常页需要over wall访问”，“图源cn就能访问”... 因此domain的都使用代理"""
     domain_regex: re.Pattern = None
