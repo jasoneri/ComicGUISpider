7dcd22fa120c6f1196e2ec23ad336e0e982310d6
json <jsoneri@outlook.com>
2024-08-18 01:56:52 +0800

    fix: deploy/update update: describe use exe for dynamically load README.md

---
+++ launcher/desc.bat
@@ -0,0 +1,15 @@
+@rem
+@echo off
+
+set "_root=%~dp0"
+set "_root=%_root:~0,-1%"
+cd "%_root%"
+@echo "%_root%
+
+set "_pyBin=%_root%\runtime"
+set "PATH=%_root%\site-packages;%_pyBin%;%PATH%"
+
+python scripts\deploy\update.py -d
+
+start "" "file:///%~dp0scripts\desc.html"
+exit
