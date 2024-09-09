4bb91dbe20271494b3ac5fec24046864608f2bce
json <jsoneri@outlook.com>
2024-09-10 01:53:42 +0800

    update: support macOS fin fix #15

--- deploy/launcher/mac/dos2unix.bash
+++ deploy/launcher/mac/dos2unix.bash
@@ -3,13 +3,7 @@ curr_p=$(cd "$(dirname "$0")";pwd)
 app_proj_p="/Applications/CGS.app/Contents/Resources/scripts"
 if [ ! -x /usr/local/bin/brew ]; then
   echo "not brew, downloading brew...";
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
-  git -C "$(brew --repo)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git;
-  git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git;
-  git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git;
-  git -C "$(brew --repo homebrew/cask-fonts)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-fonts.git;
-  git -C "$(brew --repo homebrew/cask-drivers)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-drivers.git;
-  brew update
+  /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)";
 fi
 if [ ! -x /usr/local/bin/dos2unix ]; then
   echo "not dos2unix, downloading dos2unix...";
--- deploy/launcher/mac/init.bash
+++ deploy/launcher/mac/init.bash
@@ -6,13 +6,7 @@ if [ ! -x /usr/local/bin/python3.12 ];
     then echo "无python3.12环境，正在初始化...";
     if [ ! -x /usr/local/bin/brew ]; then
       echo "not brew, downloading brew...";
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
-      git -C "$(brew --repo)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git;
-      git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git;
-      git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git;
-      git -C "$(brew --repo homebrew/cask-fonts)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-fonts.git;
-      git -C "$(brew --repo homebrew/cask-drivers)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-drivers.git;
-      brew update
+      /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)";
     fi
     brew install python@3.12;
     brew link python@3.12;
@@ -22,5 +16,5 @@ fi
 /usr/local/bin/python3.12 -m pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com --user --break-system-packages;
 
 echo ""
-echo "===== 初始化完毕，点击quit退出 ====="
+echo "===== 初始化完毕，请手动关闭终端窗口 ====="
 echo ""
