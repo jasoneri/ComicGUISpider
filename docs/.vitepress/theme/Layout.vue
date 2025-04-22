1bc50dd27c67c8995baf2170063e9d3f24ddc2ac
json <jsoneri@outlook.com>
2025-04-22 17:15:53 +0800

    docs: document organize-init

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import { useData, inBrowser } from 'vitepress'
+import { watchEffect } from 'vue'
+
+const { lang } = useData()
+watchEffect(() => {
+  if (inBrowser) {
+    document.cookie = `nf_lang=${lang.value}; expires=Mon, 1 Jan 2030 00:00:00 UTC; path=/`
+  }
+})
+</script>
+
+<template>
+  <DefaultTheme.Layout />
+</template>
