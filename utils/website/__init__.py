eb59622e6c448bdd5f0e4f0e2a6581745ee5a4d2
maki <jsonmaki@gmail.com>
2025-04-24 17:12:37 +0800

    fix: remove zstd

--- utils/website/__init__.py
+++ utils/website/__init__.py
@@ -392,7 +392,7 @@ class MangabzUtils(Utils, Req):
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1",
         "Accept": "image/avif,image/webp,image/png,image/svg+xml,image/*;q=0.8,*/*;q=0.5",
         "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Encoding": "gzip, deflate, br",
         "Referer": "https://www.mangabz.com/",
         "Sec-Fetch-Dest": "image",
         "Sec-Fetch-Mode": "no-cors",
