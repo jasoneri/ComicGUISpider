06e9cc66a57dc16c90efddf768bdb75691b05f4b
maki <jsonmaki@gmail.com>
2025-05-07 16:25:45 +0800

    refactor: crawl_only wait-flag instead of time-sleep; correctly let schedule_test test online

--- crawl_only.py
+++ crawl_only.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-"""cli，no gui,no wait for Interaction"""
+"""cli,no gui,no wait for Interaction"""
 import time
 import re
 import argparse
-from multiprocessing import Process, set_start_method
+from multiprocessing import Process, set_start_method, Queue
 
 from loguru import logger
 
@@ -15,10 +15,14 @@ from utils.processed_class import (
 from variables import SPECIAL_WEBSITES_IDXES, SPIDERS
 
 
-def say_to_textBrowser(textBrowserQueue, TasksQueue, daily_test_flag=False):
+def say_to_textBrowser(textBrowserQueue, TasksQueue, flagQueue, daily_test_flag=False):
     text_browser_q = textBrowserQueue.queue
     task_q = TasksQueue.queue
+    flag_q = flagQueue.queue
     break_flag = re.compile(f"{res.GUI.WorkThread_finish_flag}|{res.GUI.WorkThread_empty_flag}")
+    flag_patterns = (
+        res.SPIDER.chooseInput_flag, res.SPIDER.sectionInput_flag
+    )
     while 1:
         if not text_browser_q.empty():
             _state = text_browser_q.get()
@@ -27,6 +31,8 @@ def say_to_textBrowser(textBrowserQueue, TasksQueue, daily_test_flag=False):
             _ = _state.text
             if not daily_test_flag:
                 logger.debug(_)
+            if any(filter(lambda flag: flag in _, flag_patterns)):
+                flag_q.put('go')
             if bool(break_flag.search(_)):
                 break
         if not task_q.empty():
@@ -40,11 +46,22 @@ class Gui:
     def __init__(self, port):
         logger.debug(f"{port=}")
         manager = QueuesManager.create_manager(
-            'InputFieldQueue', 'TextBrowserQueue', 'ProcessQueue', 'BarQueue', 'TasksQueue',
+            'InputFieldQueue', 'TextBrowserQueue', 'ProcessQueue', 'BarQueue', 'TasksQueue', 'FlagQueue',
             address=('localhost', port), authkey=b'abracadabra'
         )
         manager.connect()
         self.Q = QueueHandler(manager)
+
+
+def wait_for_flag(flagQueue, timeout=30):
+    flag_q = flagQueue.queue
+    start_time = time.time()
+    while (time.time() - start_time) < timeout:
+        if not flag_q.empty():
+            flag = flag_q.get()
+            return True
+        time.sleep(0.1)
+    raise RuntimeError("[wait timeout] for get_flag")
 
 
 def test_turn_page():
@@ -83,17 +100,16 @@ def test_normal_process(keyword, input_2, input_3):
     state_2 = InputFieldState(keyword=keyword, bookSelected=spider_choice, indexes=input_2, pageTurn='')
     if input_3:
         state_3 = InputFieldState(keyword=keyword, bookSelected=spider_choice, indexes=input_3, pageTurn='')
-
+    flag_queue = gui.Q('FlagQueue')
     gui.Q('InputFieldQueue').send(state_1)
     refresh_state(gui, 'process_state', 'ProcessQueue', monitor_change=True)
-    time.sleep(2)
+    wait_for_flag(flag_queue)
     gui.Q('InputFieldQueue').send(state_2)
     refresh_state(gui, 'process_state', 'ProcessQueue', monitor_change=input_3 or False)
     #  上面这行 refresh_state，当测试三步跳转时要加 monitor_change=True
     if input_3:
-        time.sleep(3)
+        wait_for_flag(flag_queue)
         gui.Q('InputFieldQueue').send(state_3)
-        time.sleep(2)
 
 
 if __name__ == '__main__':
@@ -129,9 +145,10 @@ if __name__ == '__main__':
 
     spider_choice = args.website if args.website else 1  # 选网站/爬虫，转crawl_what方法一目了然
 
+    FlagQueue = Queue()
     guiQueuesManger = GuiQueuesManger()
     queue_port = guiQueuesManger.find_free_port(start_port=args.start_port)
-    p_qm = Process(target=guiQueuesManger.create_server_manager)
+    p_qm = Process(target=guiQueuesManger.create_server_manager, kwargs={"FlagQueue": FlagQueue})
     p_qm.start()
 
     try:
@@ -149,7 +166,7 @@ if __name__ == '__main__':
     p_crawler = Process(target=crawl_what, args=(spider_choice, queue_port), kwargs=p_crawler_kwargs)
     p_crawler.start()
 
-    p_bThread = Process(target=say_to_textBrowser, args=(gui.Q('TextBrowserQueue'), gui.Q('TasksQueue'), args.daily_test))
+    p_bThread = Process(target=say_to_textBrowser, args=(gui.Q('TextBrowserQueue'), gui.Q('TasksQueue'), gui.Q('FlagQueue'), args.daily_test))
     p_bThread.start()
 
     if args.turn_page:
