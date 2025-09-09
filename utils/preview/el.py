abe98c489d7eec94a22b6415a3cf5d164341ce25
json <jsoneri@outlook.com>
2025-09-09 14:10:02 +0800

    refactor: InfoMinix Ⅱ: preview

--- utils/preview/el.py
+++ utils/preview/el.py
@@ -5,19 +5,21 @@ class ElMinix:
     max_width = 170
     
     @classmethod
-    def create(cls, idx, img_src, title, url, **badges_kw):
+    def create(cls, idx, img_src, title, url, flag=None, **badges_kw):
         title = PresetHtmlEl.sub(title)
         abbreviated_title = title[:18] + "..."
         badges = Badges(**badges_kw)
-        return cls.create_(idx, img_src, title, abbreviated_title, url, badges)
+        return cls.create_(idx, img_src, title, abbreviated_title, url, badges, flag=flag)
     
     @classmethod
-    def create_(cls, idx, img_src, title, abbreviated_title, url, badges):
-        el = f"""<div class="col-md-3 singal-task" style="max-width:{cls.max_width}px"><div class="form-check">
+    def create_(cls, idx, img_src, title, abbreviated_title, url, badges, flag=None):
+        container_cls = f" container-{flag}" if flag else ""
+        img_cls = f" img-{flag}" if flag else ""
+        el = f"""<div class="col-md-3 singal-task" style="max-width:{cls.max_width}px"><div class="form-check{container_cls}">
         <input class="form-check-input" type="checkbox" name="img" id="{idx}">
         <label class="form-check-label" for="{idx}">
             <div style="position: relative; display: inline-block;">
-                <img src="{img_src}" title="{title}" alt="{title}" class="img-thumbnail"/>
+                <img src="{img_src}" title="{title}" alt="{title}" class="img-thumbnail{img_cls}"/>
                 {badges}
             </div>
         </label></div>
