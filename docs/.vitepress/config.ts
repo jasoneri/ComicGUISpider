e5952763a383cb6771f9546e640e34a2306455a5
json <jsoneri@outlook.com>
2026-02-08 15:54:11 +0800

    build: v2.8.3

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vitepress';
 
 
-const version = `v2.8.2`
+const version = `v2.8.3`
 
 // https://vitepress.dev/reference/site-config
 // @ts-ignore
--- docs/_github/release_notes.md
+++ docs/_github/release_notes.md
@@ -2,4 +2,4 @@
 ## 🐞 Fix
 
 同 `v2.8.1` ， 见[参考](https://doc.comicguispider.nyc.mn/changelog/history)  
-发现 tag 包替换无法打开所以转 pypi 发布 `2.8.2` ，后续应该恢复 pypi  
+恢复 pypi (之前的更新方式)  
