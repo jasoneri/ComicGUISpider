617da6a5d6affa2a08085f3516edb60e3f6344d4
json <jsoneri@outlook.com>
2026-09-14 19:50:35 +0800

    build: v2.12.1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,7 +6,7 @@ import { markdownUrlReplacePlugin } from './plugins/markdown-url-replace';
 import { createDocsUrlConfig } from './shared/urls';
 
 
-const version = `v2.12.1-beta`
+const version = `v2.12.1`
 const docsRoot = resolve(dirname(fileURLToPath(import.meta.url)), '..')
 const docsEnv = loadEnv('', docsRoot, '')
 const { URLS, PLACEHOLDER_MAP } = createDocsUrlConfig(docsEnv)
